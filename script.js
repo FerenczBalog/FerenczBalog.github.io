9a66200169ceb9ef8ce49dcedea4be5d4b842325
Balog Ferencz <90491641+FerenczBalog@users.noreply.github.com>
2023-05-30 23:08:11 +0300

    Setting up the new portfolio
    > project setup
    > building the Hero page
    > building the navigation

---
+++ script.js
@@ -0,0 +1,23 @@
+//          DOM <
+const modal = document.querySelector("#navigation-box");
+const openModal = document.querySelector("#hamburger-container");
+const overlay = document.querySelector("#overlay")
+//          DOM >
+
+//      MODAL <
+openModal.onclick = () => {
+    modal.style.display = "flex"
+    overlay.style.display = "block"
+    openModal.style.display = "none"
+}
+overlay.onclick = () => {
+    modal.style.display = "none"
+    overlay.style.display = "none"
+    openModal.style.display = "block"
+}
+modal.onclick = () => {
+    modal.style.display = "none"
+    overlay.style.display = "none"
+    openModal.style.display = "block"
+}
+//      MODAL >
