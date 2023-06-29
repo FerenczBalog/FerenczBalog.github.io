6a053af55d48111d60882d27ab87e35f8d60049f
ferenczbalog <balogferencz.artist@gmail.com>
2023-06-29 10:05:42 +0300

    .

--- script.js
+++ script.js
@@ -9,6 +9,23 @@ const toWebsite = document.querySelector(".to-website-container");
 //  navigation modal
 //          DOM >
 
+//    SLIDES <
+const slides = [
+  {
+    title: 'Slide 1',
+    content: 'Content 1'
+  },
+  {
+    title: 'Slide 2',
+    content: 'Content 1'
+  },
+  {
+    title: 'Slide 2',
+    content: 'Content 1'
+  }
+]
+//    SLIDES >
+
 //      MODAL <
 openModal.onclick = () => {
     modal.style.display = "flex"
@@ -40,30 +57,24 @@ thumbimg.onmouseout = () => {
 
 
 // slideshow <
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = [1,1,1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides(n, no) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  x[slideIndex[no]-1].style.display = "block";  
 }
 //slideshow >
