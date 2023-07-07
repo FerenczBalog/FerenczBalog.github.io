3520777b8fd0af41602db9a1df9a3a723e84503f
ferenczbalog <balogferencz.artist@gmail.com>
2023-07-07 10:58:08 +0300

    >>>

--- script.js
+++ script.js
@@ -9,23 +9,6 @@ const toWebsite = document.querySelector(".to-website-container");
 //  navigation modal
 //          DOM >
 
-//    SLIDES <
-const slides = [
-  {
-    title: 'Slide 1',
-    content: 'Content 1'
-  },
-  {
-    title: 'Slide 2',
-    content: 'Content 1'
-  },
-  {
-    title: 'Slide 2',
-    content: 'Content 1'
-  }
-]
-//    SLIDES >
-
 //      MODAL <
 openModal.onclick = () => {
     modal.style.display = "flex"
@@ -54,27 +37,3 @@ thumbimg.onmouseout = () => {
     toWebsite.style.display = "none"
 }
 //      TO THE WEBSITE  <
-
-
-// slideshow <
-let slideIndex = [1,1,1];
-let slideId = ["mySlides1", "mySlides2", "mySlides3"]
-showSlides(1, 0);
-showSlides(1, 1);
-showSlides(1, 2);
-
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
-
-function showSlides(n, no) {
-  let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex[no]-1].style.display = "block";  
-}
-//slideshow >
